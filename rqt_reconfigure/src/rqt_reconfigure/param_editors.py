5c7cfcc6a41bb23ffece6d64df7653dddcfa767d
Dirk Thomas <dthomas@osrfoundation.org>
2013-09-20 18:09:22 -0700

    Rebase from 'upstream'

--- rqt_reconfigure/src/rqt_reconfigure/param_editors.py
+++ rqt_reconfigure/src/rqt_reconfigure/param_editors.py
@@ -63,43 +63,53 @@ ui_enum = os.path.join(rp.get_path('rqt_reconfigure'), 'resource',
 
 
 class EditorWidget(QWidget):
+    '''
+    This class is abstract -- its child classes should be instantiated.
+
+    There exist two kinds of "update" methods:
+    - _update_paramserver for Parameter Server.
+    - update_value for the value displayed on GUI.
+    '''
 
     def __init__(self, updater, config):
-        """
-        :param updater:
-        :type updater: rqt_reconfigure.param_updater.ParamUpdater
-        """
+        '''
+        @param updater: A class that extends threading.Thread.
+        @type updater: rqt_reconfigure.param_updater.ParamUpdater
+        '''
 
         super(EditorWidget, self).__init__()
 
-        self.updater = updater
+        self._updater = updater
         self.param_name = config['name']
 
         self.old_value = None
 
-    def _update(self, value):
+    def _update_paramserver(self, value):
+        '''
+        Update the value on Parameter Server.
+        '''
         if value != self.old_value:
             self.update_configuration(value)
             self.old_value = value
 
     def update_value(self, value):
-        """
+        '''
         To be overridden in subclass.
 
-        Update the value of the arbitrary components based on user's input.
-        """
+        Update the value that's displayed on the arbitrary GUI component
+        based on user's input.
+        '''
         pass
 
     def update_configuration(self, value):
-        self.updater.update({self.param_name: value})
+        self._updater.update({self.param_name: value})
 
     def display(self, grid):
-        """
+        '''
         Should be overridden in subclass.
 
         :type grid: QFormLayout
-        :type row: ???
-        """
+        '''
         self._paramname_label.setText(self.param_name)
 #        label_paramname = QLabel(self.param_name)
 #        label_paramname.setWordWrap(True)
@@ -107,9 +117,9 @@ class EditorWidget(QWidget):
         grid.addRow(self._paramname_label, self)
 
     def close(self):
-        """
+        '''
         Should be overridden in subclass.
-        """
+        '''
         pass
 
 
@@ -119,7 +129,9 @@ class BooleanEditor(EditorWidget):
         loadUi(ui_bool, self)
 
         self.update_value(config['default'])
-        self._checkbox.clicked.connect(self._update)
+        self._checkbox.clicked.connect(self._update_paramserver)
+        #TODO: Maybe add slot for stateChanged; it can be just:
+        #      self._checkbox.stateChanged.connect(self._update_paramserver)
 
     def update_value(self, value):
         self._checkbox.setChecked(value)
@@ -130,7 +142,11 @@ class StringEditor(EditorWidget):
         super(StringEditor, self).__init__(updater, config)
         loadUi(ui_str, self)
 
+        # Emit signal when cursor leaves the text field.
         self._paramval_lineedit.editingFinished.connect(self.edit_finished)
+        # Add textChanged to capture the change while cursor is still in
+        # the text field.
+        self._paramval_lineedit.textChanged.connect(self.edit_finished)
 
     def update_value(self, value):
         rospy.logdebug('StringEditor update_value={}'.format(value))
@@ -139,7 +155,7 @@ class StringEditor(EditorWidget):
     def edit_finished(self):
         rospy.logdebug('StringEditor edit_finished val={}'.format(
                                               self._paramval_lineedit.text()))
-        self._update(self._paramval_lineedit.text())
+        self._update_paramserver(self._paramval_lineedit.text())
 
 
 class IntegerEditor(EditorWidget):
@@ -148,45 +164,50 @@ class IntegerEditor(EditorWidget):
 
         loadUi(ui_int, self)
 
-        self.min = int(config['min'])
-        self.max = int(config['max'])
-        self._min_val_label.setText(str(self.min))
-        self._max_val_label.setText(str(self.max))
+        self._min = int(config['min'])
+        self._max = int(config['max'])
+        self._min_val_label.setText(str(self._min))
+        self._max_val_label.setText(str(self._max))
 
-        self._slider_horizontal.setRange(self.min, self.max)
-        self._slider_horizontal.sliderReleased.connect(self.slider_released)
-        self._slider_horizontal.sliderMoved.connect(self.update_text)
-        # valueChanged gets called when groove is clicked on.
-        self._slider_horizontal.valueChanged.connect(self.update_value)
+        self._slider_horizontal.setRange(self._min, self._max)
 
         # TODO: Fix that the naming of _paramval_lineEdit instance is not
         #       consistent among Editor's subclasses.
-        self._paramval_lineEdit.setValidator(QIntValidator(self.min,
-                                                           self.max, self))
-        self._paramval_lineEdit.editingFinished.connect(self.editing_finished)
+        self._paramval_lineEdit.setValidator(QIntValidator(self._min,
+                                                           self._max, self))
+
+        # Connect slots
+        self._paramval_lineEdit.textChanged.connect(self._text_edited)
+        self._slider_horizontal.sliderReleased.connect(self._slider_released)
+        #self._slider_horizontal.sliderMoved.connect(self._update_text_gui)
+        # valueChanged gets called when groove is clicked on.
+        #self._slider_horizontal.valueChanged.connect(self.update_value)
 
         # TODO: This should not always get set to the default it should be the
         # current value
         self._paramval_lineEdit.setText(str(config['default']))
         self._slider_horizontal.setSliderPosition(int(config['default']))
 
-    def slider_released(self):
-        self.update_text(self._slider_horizontal.value())
-        self._update(self._slider_horizontal.value())
+    def _text_edited(self):
+        self._update_paramserver(int(self._paramval_lineEdit.text()))
 
-    def update_text(self, val):
-        rospy.logdebug(' IntegerEditor.update_text val=%s', str(val))
-        self._paramval_lineEdit.setText(str(val))
+    def _slider_released(self):
+        '''Slot for mouse being released from slider.'''
+        _slider_val = self._slider_horizontal.value()
+        self._update_text_gui(_slider_val)
 
-    def editing_finished(self):
-        self._slider_horizontal.setSliderPosition(
-                                         int(self._paramval_lineEdit.text()))
-        self._update(int(self._paramval_lineEdit.text()))
+    def _update_text_gui(self, value):
+        rospy.logdebug(' IntegerEditor._update_text_gui val=%s', str(value))
+        self._paramval_lineEdit.setText(str(value))
+        # Run self._update_paramserver to update the value on PServer
+        self._update_paramserver(value)
+        self.update_value(value)
 
     def update_value(self, val):
-        self._slider_horizontal.setSliderPosition(int(val))
+        # Can be redundant when the trigger is the move of slider.
+        self._slider_horizontal.setSliderPosition(val)
+
         self._paramval_lineEdit.setText(str(val))
-        rospy.logdebug(' IntegerEditor.update_val val=%s', str(val))
 
 
 class DoubleEditor(EditorWidget):
@@ -197,69 +218,78 @@ class DoubleEditor(EditorWidget):
 
         # Handle unbounded doubles nicely
         if config['min'] != -float('inf'):
-            self.min = float(config['min'])
-            self._min_val_label.setText(str(self.min))
-            self.func = lambda x: x
-            self.ifunc = self.func
+            self._min = float(config['min'])
+            self._min_val_label.setText(str(self._min))
+            self._func = lambda x: x
+            self._ifunc = self._func
         else:
-            self.min = -1e10000
+            self._min = -1e10000
             self._min_val_label.setText('-inf')
-            self.func = lambda x: math.atan(x)
-            self.ifunc = lambda x: math.tan(x)
+            self._func = lambda x: math.atan(x)
+            self._ifunc = lambda x: math.tan(x)
 
         if config['max'] != float('inf'):
-            self.max = float(config['max'])
-            self._max_val_label.setText(str(self.max))
-            self.func = lambda x: x
-            self.ifunc = self.func
+            self._max = float(config['max'])
+            self._max_val_label.setText(str(self._max))
+            self._func = lambda x: x
+            self._ifunc = self._func
         else:
-            self.max = 1e10000
+            self._max = 1e10000
             self._max_val_label.setText('inf')
-            self.func = lambda x: math.atan(x)
-            self.ifunc = lambda x: math.tan(x)
+            self._func = lambda x: math.atan(x)
+            self._ifunc = lambda x: math.tan(x)
 
-        self.scale = (self.func(self.max) - self.func(self.min)) / 100
+        self.scale = (self._func(self._max) - self._func(self._min)) / 100
         if self.scale == 0:
             self.setDisabled(True)
 
-        self.offset = self.func(self.min)
+        self.offset = self._func(self._min)
 
-        self._slider_horizontal.setRange(self.slider_value(self.min),
-                                         self.slider_value(self.max))
-        self._slider_horizontal.sliderReleased.connect(self.slider_released)
-        self._slider_horizontal.sliderMoved.connect(self.update_text)
+        self._slider_horizontal.setRange(self._get_value_slider(self._min),
+                                         self._get_value_slider(self._max))
 
         self._paramval_lineEdit.setValidator(QDoubleValidator(
-                                                    self.min, self.max,
+                                                    self._min, self._max,
                                                     8, self))
-        self._paramval_lineEdit.editingFinished.connect(self.editing_finished)
-
         self._paramval_lineEdit.setText(str(config['default']))
         self._slider_horizontal.setSliderPosition(
-                                     self.slider_value(config['default']))
+                                     self._get_value_slider(config['default']))
 
-    def get_value(self):
-        return self.ifunc(self._slider_horizontal.value() * self.scale)
+        # Connect slots
+        self._paramval_lineEdit.textChanged.connect(self._text_edited)
+        self._slider_horizontal.sliderReleased.connect(self._slider_released)
 
-    def slider_value(self, value):
-        return int(round((self.func(value)) / self.scale)) if self.scale else 0
+    def _text_edited(self):
+        '''Slot for text changed event in text field.'''
+        self._update_paramserver(float(self._paramval_lineEdit.text()))
 
-    def slider_released(self):
-        self.update_text(self.get_value())
-        self._update(self.get_value())
+    def _get_value_textfield(self):
+        '''@return: Current value in text field.'''
+        return self._ifunc(self._slider_horizontal.value() * self.scale)
 
-    def update_text(self, value):
-        self._paramval_lineEdit.setText(str(self.get_value()))
-        rospy.logdebug(' DblEditor.update_text val=%s', str(value))
+    def _slider_released(self):
+        '''Slot for mouse being released from slider.'''
+        _slider_val = self._get_value_textfield()
+        self._update_text_gui(_slider_val)
 
-    def editing_finished(self):
-        self._slider_horizontal.setSliderPosition(
-                      self.slider_value(float(self._paramval_lineEdit.text())))
-        self._update(float(self._paramval_lineEdit.text()))
+    def _get_value_slider(self, value):
+        '''
+        @rtype: double
+        '''
+        return int(round((self._func(value)) / self.scale)) if self.scale else 0
+
+    def _update_text_gui(self, value):
+        self._paramval_lineEdit.setText(str(value))
+        rospy.loginfo(' DblEditor._update_text_gui val=%s', str(value))
+        # Run self._update_paramserver to update the value on PServer
+        self._update_paramserver(value)
+        self.update_value(value)
 
     def update_value(self, val):
+        # Can be redundant when the trigger is the move of slider.
         self._slider_horizontal.setSliderPosition(
-                                  self.slider_value(float(val)))
+                                            self._get_value_slider(float(val)))
+
         self._paramval_lineEdit.setText(str(val))
 
 
@@ -285,8 +315,13 @@ class EnumEditor(EditorWidget):
         self._combobox.currentIndexChanged['int'].connect(self.selected)
 
     def selected(self, index):
-        self._combobox.setCurrentIndex(self.values[index])
-        self._update(self.values[index])
+        self._update_paramserver(self.values[index])
 
-    #def update_value(self, val):
-    #    self._combobox.setCurrentIndex(self.values.index(val))
+    def update_value(self, val):
+        # apparently, when the currentIndexChanged signal is emitted, current 
+        # index internally is still the old value, why the setCurrentIndex call
+        # below triggers another selected() call ... couldn't fin a better 
+        # solution than this. 
+        self._combobox.currentIndexChanged['int'].disconnect()
+        self._combobox.setCurrentIndex(self.values.index(val))
+        self._combobox.currentIndexChanged['int'].connect(self.selected)
